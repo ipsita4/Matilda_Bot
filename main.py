61650388f7823ac7dcda2bc1aa194a39b88ee8d3
Ipsita Bhattacharya <59875404+ipsita4@users.noreply.github.com>
2021-10-22 14:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from play import music_cog
+from commands import main_cog
 import discord
 from discord.ext import commands
 import dotenv
@@ -5,29 +7,28 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-#import all of the cogs
-from commands import main_cog
+# import all of the cogs
 #from image_cog import image_cog
-from play import music_cog
 
 bot = commands.Bot(command_prefix='>')
 
-#remove the default help command so that we can write out own
+# remove the default help command so that we can write out own
 bot.remove_command('help')
 
-#register the class with the bot
+# register the class with the bot
 bot.add_cog(main_cog(bot))
-#bot.add_cog(image_cog(bot))
+# bot.add_cog(image_cog(bot))
 bot.add_cog(music_cog(bot))
 
-@bot.event
-async def on_ready():
-    print('Hello, I am your music bot {0.user}'
-    .format(bot))
-    general_channel= bot.get_channel(882939306870714408)
+# @bot.event
+# async def on_ready():
+# print('Hello, I am your music bot {0.user}'
+# .format(bot))
+#general_channel= bot.get_channel(os.getenv('ID'))
 
-    await general_channel.send('Hello, I am your music bot {0.user}'
-    .format(bot))
+# await general_channel.send('Hello, I am your music bot {0.user}'
+# .format(bot))
 
-#start the bot with our token
+
+# start the bot with our token
 bot.run(os.getenv('TOKEN'))
